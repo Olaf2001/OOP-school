71f07986cc4ccea67fc2750b26f3ebd963522386
unknown <olafkramer2001@gmail.com>
2019-02-04 11:57:19 +0100

    de vorige opdracht overgenomen en er een class Room bij gemaakt

---
+++ Opdracht 1b/class/House.php
@@ -0,0 +1,34 @@
+<?php
+class House 
+{
+    private $floors;
+    private $rooms;
+    private $width;
+    private $length;
+    private $height;
+    private $priceCubicMeter;
+    private $size;
+    private $price;
+    public function __construct($floors, $rooms, $width, $length, $height, $priceCubicMeter)
+    {
+        $this->floors = $floors;
+        $this->rooms = $rooms;
+        $this->width = $width;
+        $this->length = $length;
+        $this->height = $height;
+        $this->priceCubicMeter = $priceCubicMeter;
+        $this->size = $this->width * $this->length * $this->height;
+        $this->price = $this->size * $this->priceCubicMeter;
+    }
+    public function getHouse()
+    {
+        return "aantal verdiepingen = ".$this->floors."<br>
+                aantal kamers = ".$this->rooms."<br>
+                breedte = ".$this->width." meter <br>
+                lengte = ".$this->length." meter <br>
+                hoogte = ".$this->height." meter <br>
+                inhoud = ".$this->size." m<sup>3</sup> <br>
+                prijs = &euro; ".$this->price." <br>"
+                ;
+    }
+}
---
+++ Opdracht 1b/class/Room.php
@@ -0,0 +1,25 @@
+<?php
+class Room
+{
+    private $width;
+    private $length;
+    private $height;
+    private $size;
+    
+    public function __construct($width, $length, $height)
+    {
+        $this->width = $width;
+        $this->length = $length;
+        $this->height = $height;
+        $this->size = $this->width * $this->length * $this->height;
+    }
+    
+    public function getRoom()
+    {
+        return "breedte = ".$this->width." meter <br>
+                lengte = ".$this->length." meter <br>
+                hoogte = ".$this->height." meter <br>
+                inhoud = ".$this->size." m<sup>3</sup> <br>"
+                ;
+    }
+}

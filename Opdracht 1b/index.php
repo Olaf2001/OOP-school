3b48f37d3babe4286ce9ba9ec59ef5c0cc41e9c2
unknown <olafkramer2001@gmail.com>
2019-02-05 20:20:31 +0100

    verder geprobeerd de opdracht te maken en niet gelukt, maar commentaar er ingezet van wat ik wil doen en foutmeldingen

--- Opdracht 1b/index.php
+++ Opdracht 1b/index.php
@@ -4,9 +4,9 @@ class AllRooms
 {
     private $allRooms = [];
 
-    public function addAllRooms($room)
+    public function addAllRooms($sizeOfRoom)
     {
-        $this->allRooms[] = $rooms;
+        $this->allRooms[] = $sizeOfRoom;
     }
 }
 
@@ -15,34 +15,43 @@ class House
     private $floors;
     private $rooms;
     private $priceCubicMeter;
-    private $price;   
+    private $size;
+    private $price;
     
-    public function setHouse($floors, $rooms, $priceCubicMeter)
+    public function __construct($floors, $rooms, $size, $priceCubicMeter)
     {
         $this->floors = $floors;
         $this->rooms = $rooms;
         $this->priceCubicMeter = $priceCubicMeter;
-        $this->price = $this->addAllRooms * $this->priceCubicMeter;
+        $this->size = $size;
+        $this->price = $rooms * $this->priceCubicMeter;
+        // Notice: Object of class AllRooms could not be converted to int in D:\school\Xampp\htdocs\bestanden\
+        // school\OOP basis\OOP opdrachten\Opdracht 1b\index.php on line 27 Huis 1
     }
 
     public function getHouse()
     {
         return "aantal verdiepingen = ".$this->floors."<br>
                 aantal kamers = ".$this->rooms."<br>
-                inhoud = ".$this->size." m<sup>3</sup> <br>
+                Recoverable fatal error: Object of class AllRooms could not be converted to string in D:\school\Xampp\
+                htdocs\bestanden\school\OOP basis\OOP opdrachten\Opdracht 1b\index.php on line 33
+                inhoud = ".$this->size."<br>
                 prijs = &euro; ".$this->price." <br>"
                 ;
     }
+
+//Recoverable fatal error: Object of class AllRooms could not be converted to string in D:\school\Xampp\htdocs\
+//bestanden\school\OOP basis\OOP opdrachten\Opdracht 1b\index.php on line 35
 }
 
-class Room extends House
+class Room
 {
     private $width;
     private $length;
     private $height;
     private $size;
     
-    public function setRoom($width, $length, $height)
+    public function __construct($width, $length, $height)
     {
         $this->width = $width;
         $this->length = $length;
@@ -52,15 +61,20 @@ class Room extends House
     
     public function getRoom()
     {
-        return "breedte = ".$this->width." meter <br>
-                lengte = ".$this->length." meter <br>
-                hoogte = ".$this->height." meter <br>
-                inhoud = ".$this->size." m<sup>3</sup> <br>"
-                ;
+        return $this->size;
     }
 }
 
-$house1 = new Room(2, 3, 4);
-$house1->setRoom(2, 2, 2);
+$room1 = new Room(3, 3, 3);
+$room2 = new Room(3, 3, 3);
+$room3 = new Room(2, 3, 4);
+
+$allRooms1 = new AllRooms();
+$allRooms1->addAllRooms($room1->getRoom());
+// Kan maar 1 waarde invullen, alleen wil alle kamers in kunnen vullen zodat de grootte kan worden bepaald van het
+// huis/
+
+$house1 = new House(2, $allRooms1, $allRooms1, 4);
+
 print '<strong>Huis 1</strong> <br>';
 print $house1->getHouse();

b32be260db7d48b31b11503a54e136c869f516ff
unknown <olafkramer2001@gmail.com>
2019-02-06 14:11:16 +0100

    de index netjes gemaakt

--- Opdracht 1b/index.php
+++ Opdracht 1b/index.php
@@ -3,17 +3,15 @@
 include('./class/House.php');
 include('./class/Room.php');
 
-$house1 = new House(3, 10);
-
 $room1 = new Room(3, 3, 3);
 $room2 = new Room(5, 4, 3);
 $room3 = new Room(10, 5, 3);
 
-$house2 = new House(3, 20);
-
+$house1 = new House(3, 10);
 $house1->addRoom($room1->getVolume());
 $house1->addRoom($room2->getVolume());
 
+$house2 = new House(3, 20);
 $house2->addRoom($room1->getVolume());
 
 print '<strong>Huis 1</strong> <br>';

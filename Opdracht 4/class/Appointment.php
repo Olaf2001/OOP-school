ed7723d87b3f5157720a8349f76c034bbbf3bfb1
unknown <olafkramer2001@gmail.com>
2019-03-11 09:52:55 +0100

    de nurse optioneel gemaakt, de rest is nog niet gelukt

--- Opdracht 4/class/Appointment.php
+++ Opdracht 4/class/Appointment.php
@@ -7,16 +7,25 @@ class Appointment
     // een functie om een nurse, doctor en patient toe te voegen aan de afspraak :)
     private $doctor;
     private $patient;
+    private $startTime;
+    private $endTime;
+    private $nurse;
 
-    public function __construct($doctor, $patient)
+    public function __construct($doctor, $patient, $startTime, $endTime, $nurse = '')
     {
         $this->doctor = $doctor;
         $this->patient = $patient;
+        $this->startTime = $startTime;
+        $this->endTime = $endTime;
+        $this->nurse = $nurse;
+        // $this->timeDifference = $startTime->diff($endTime); tijden lukken nog even niet
     }
 
     public function getPersons()
     {
-        return $this->doctor.'<br>'.$this->patient;
+        return $this->doctor.'<br>'.$this->patient.'<br>'.$this->nurse.'<br>'.$this->startTime.'<br>'.$this->endTime;
     }
 
 }
+
+// geen idee hoe je static moet gebruiken want het moet juist dynamisch worden en daar gebruik je geen static voor
--- Opdracht 4/index.php
+++ Opdracht 4/index.php
@@ -11,6 +11,6 @@ $patient1 = new Patient('Tim','Smitz','blue', 'blond','1.85', '70kg');
 $doctor1 = new Doctor('Zoute', 'Pinda','blue', 'blond', '1,79','58kg');
 $nurse1 = new Nurse('Meneer','de Entiteit','blue','blond','1.82','66kg');
 
-$appointment1 = new Appointment($doctor1->seePerson(), $patient1->seePerson());
+$appointment1 = new Appointment($doctor1->seePerson(), $patient1->seePerson(), '10:05:00', '11:55:00', $nurse1->seePerson());
 
 print $appointment1->getPersons();

ba31831136fa2477a293e999c1efb415e30cd809
unknown <olafkramer2001@gmail.com>
2019-03-13 12:11:33 +0100

    verder geprobeerd om de tijd uit te rekenen

--- Opdracht 4/class/Appointment.php
+++ Opdracht 4/class/Appointment.php
@@ -7,23 +7,36 @@ class Appointment
     // een functie om een nurse, doctor en patient toe te voegen aan de afspraak :)
     private $doctor;
     private $patient;
-    private $startTime;
-    private $endTime;
+    private $startTimeHour;
+    private $startTimeMinutes;
+    private $endTimeHour;
+    private $endTimeMinutes;
     private $nurse;
 
-    public function __construct($doctor, $patient, $startTime, $endTime, $nurse = '')
+    public function __construct($doctor, $patient, $nurse = '')
     {
         $this->doctor = $doctor;
         $this->patient = $patient;
-        $this->startTime = $startTime;
-        $this->endTime = $endTime;
         $this->nurse = $nurse;
         // $this->timeDifference = $startTime->diff($endTime); tijden lukken nog even niet
     }
 
+    public function setTime($startTimeHour, $startTimeMinutes , $endTimeHour, $endTimeMinutes)
+    {
+     $this->startTime = date("h:i", mktime($startTimeHour, $startTimeMinutes));
+     $this->endTime = date("h:i", mktime($endTimeHour, $endTimeMinutes));
+    }
+    
+    public function getTime()
+    {
+        $time = date_diff($this->endTime, $this->startTime) ;
+
+        return $this->startTime.'<br>'.$this->endTime.'<br>'.$time;
+    }
     public function getPersons()
     {
-        return $this->doctor.'<br>'.$this->patient.'<br>'.$this->nurse.'<br>'.$this->startTime.'<br>'.$this->endTime;
+        return $this->doctor.'<br>'.$this->patient.'<br>'.$this->nurse.'<br>';
+        // .$this->startTime.'<br>'.$this->endTime;
     }
 
 }

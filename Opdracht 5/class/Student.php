5c814addc76fd94c34df71574d3eb92e9bd9afec
unknown <olafkramer2001@gmail.com>
2019-03-16 20:30:28 +0100

    tijdelijke commit

--- Opdracht 5/class/Student.php
+++ Opdracht 5/class/Student.php
@@ -7,9 +7,14 @@ class Student extends Person
     private $paid;
     private $className;
 
-    public function setPayment($paid)
+    public function setPayment($paid = 'nog niet betaald')
     {
         $this->paid = $paid;
+    }
+
+    public function addGroup($className)
+    {
+        $this->className = $className;
     }
 
     public function getPayment()
--- Opdracht 5/index.php
+++ Opdracht 5/index.php
@@ -8,6 +8,31 @@ use OOP_basis_Opdracht_5\Group;
 use OOP_basis_Opdracht_5\Schooltrip;
 use OOP_basis_Opdracht_5\SchooltripList;
 
-$student1 = new Student('Olaf','Kramer');
+$student1 = new Student('Giel','Houtepen');
+$student1->setPayment(5);
+$student2 = new Student('Jochem','Wortel');
+$student1->setPayment();
+$student3 = new Student('Dirk','de Beer');
+$student1->setPayment(10);
+
+$group1 = new Group('APM2A');
+
+$group1->addStudent($student1);
+$student1->addGroup($group1);
+$group1->addStudent($student2);
+$student2->addGroup($group1);
+$group1->addStudent($student3);
+$student3->addGroup($group1);
+
+$teacher1 = new Teacher('Jan','Slim');
+
+$schooltripList1 = new SchooltripList();
+$schooltripList1->addStudent($student1);
+$schooltripList1->addStudent($student2);
+$schooltripList1->addStudent($student3);
+$schooltripList1->addTeacher($teacher1);
+
+$schooltrip1 = new Schooltrip('pretpark HappyLand');
+$schooltrip1->addSchoolTripList($schooltripList1);
 
 print $student1->getName();
